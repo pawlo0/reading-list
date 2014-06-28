38457b7623573259c9a63369d5350090dd1d4af2
pawlo0 <paulo_sss@hotmail.com>
2014-06-28 11:35:46 +0100

    Revert "Added finished and recent scopes to Book"
    This reverts commit 18855277c573b092ab4e6426ce9a7c3292945727.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,5 @@
 class Book < ActiveRecord::Base
 
- scope :finished, ->{where.not(finished_on: nil)}
- scope :recent,->{where('finished_on >= ?', 1.year.ago)}
-
  def finished?
    finished_on.present?
  end
